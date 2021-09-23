38457d98d3c820028e37f63f1e68d74992cf944a
alexfabianojr <alexfabianojr@gmail.com>
2021-09-23 11:53:27 -0300

    comandos terminal

---
+++ aula2-comandos-terminal/comandos.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+//este codigo deve conter erros
+
+func main() {
+	fmt.Printf("Outro programa em %s!")
+}
+
+//comando 'go' mostra varias possibilidades de comandos
+
+// comando: go help [algumComando] - explica o que o comando fornecido faz
+// comando: go env - informacoes de configuracao do go local
+// comando: go doc cmd/vet - forma simplificada de verificar codigo 'morto' ex: go vet comandos.go
+// comando: go build ex: go build comandos.go
+// comando: go run ex: go run comandos.go
